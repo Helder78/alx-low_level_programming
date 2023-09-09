127500e6c28524df48069dfa3b75f89378036254
Helder <egbewole78@gmail.com>
2023-09-09 02:42:09 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main ()
+{
+//my code goes here
+  for (int x = 'A'; x < 'Z'; x++)
+    {
+      x = tolower(x);
+      putchar(x);
+    }
+  return (0);
+}
+
+  
