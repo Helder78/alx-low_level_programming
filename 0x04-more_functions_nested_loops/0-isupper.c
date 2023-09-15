7609ee0baaf1d2908ce621f978acffc5d39a97fe
Feranmi Egbewole <egbewole78@gmail.com>
2023-09-15 05:44:26 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ * main - Checking fo upper case or otherwise
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
