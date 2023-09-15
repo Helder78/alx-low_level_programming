4c397f14707acc356c368ce701d5014bf853f161
Feranmi Egbewole <egbewole78@gmail.com>
2023-09-15 06:46:59 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  main - function thst multiply two integerand return result
+ *
+ *  Return : Always (Success);
+ */
+int mul(int a, int b)
+{
+	int i;
+	result = a * b;
+
+	return (i);
+}
